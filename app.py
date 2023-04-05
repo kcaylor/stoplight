e5ac62c6dd4a4e57b8379f8c4efc7c9bf7d1c3d3
Kelly Caylor <caylor@ucsb.edu>
2023-04-04 18:54:40 -0700

    updated app routing

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def get_status_summary():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
