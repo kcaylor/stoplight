c88cfbe2750aee80f74f4c1c80663a4ed4381171
Kelly Caylor <caylor@ucsb.edu>
2023-04-04 19:54:05 -0700

    remove max updates

--- app.py
+++ app.py
@@ -20,24 +20,23 @@ def index():
 
 @app.route('/api/status', methods=['POST'])
 def post_status():
-    global status_updates
+    status_updates
     status = request.json.get('status')
     timestamp = time.time()
     status_updates.append({'status': status, 'timestamp': timestamp})
-    if len(status_updates) > max_updates:
-        # If there are more than 450 status updates, remove the oldest ones
-        status_updates = status_updates[-max_updates:]
-        return jsonify({'message': 'Too many updates in past 10 minutes'})
-    else:
-        return jsonify({'message': 'Status update received'})
+    # if len(status_updates) > max_updates:
+    #     # If there are more than 450 status updates, remove the oldest ones
+    #     status_updates = status_updates[-max_updates:]
+    #     return jsonify({'message': 'Too many updates in past 10 minutes'})
+    # else:
+    return jsonify({'message': 'Status update received'})
 
 
 @app.route('/api/status_summary', methods=['GET'])
 def get_status_summary():
-    global status_updates
+    status_updates
     cutoff_time = time.time() - 600  # 10 minutes ago
     recent_updates = [update for update in status_updates if update['timestamp'] >= cutoff_time]
-
     red_count = sum(1 for update in recent_updates if update['status'] == 'red')
     green_count = sum(1 for update in recent_updates if update['status'] == 'green')
     yellow_count = sum(1 for update in recent_updates if update['status'] == 'yellow')
